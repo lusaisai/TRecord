8ea287b2be1336235d7106b52ed719657258a3c9
Sam <samlu1@ebay.com>
2019-09-10 06:21:51 -0700

    support ddl fetch for non priviledged user

--- trecord/oracle.py
+++ trecord/oracle.py
@@ -70,6 +70,13 @@ class CxOracle(Database):
     def get_current_db(self) -> str:
         return ''
 
+    def check_dict_privs(self) -> bool:
+        count = self.query(f"select count(*) from USER_SYS_PRIVS where PRIVILEGE like '%SELECT%DICTIONARY%'")[0][0]
+        if count > 0:
+            return True
+        else:
+            return False
+
     def get_tables(self, database=None) -> list:
         if not database:
             database = self.database_url.username
@@ -80,6 +87,69 @@ class CxOracle(Database):
         if not database:
             database = self.database_url.username
 
+        ddl = None
+
+        if self.check_dict_privs():
+            ddl = self.get_ddl_from_meta(table, database)
+        else:
+            ddl = self.create_ddl(table, database)
+
+        return ddl
+
+    def create_ddl(self, table, database):
+        ddl = f'CREATE TABLE {database}.{table} (\n'
+
+        metadata = self.query(f"select column_name, data_type, data_length, data_precision, data_scale, nullable \
+            from all_tab_columns where upper(table_name) = upper('{table}') and upper(owner) = upper('{database}') order by column_id;")
+
+        for index, column in enumerate(self.column_list_for_ddl(metadata)):
+            if index == 0:
+                ddl += ('  {} {}\n'.format(column[0], column[1]))
+            else:
+                ddl += (', {} {}\n'.format(column[0], column[1]))
+
+        ddl += ')\n;\n\n'
+
+        for index_name, unique in self.get_indexes(table, database):
+            if unique == 'NONUNIQUE':
+                ddl += f"CREATE INDEX {index_name} ON {database}.{table} ({','.join(self.get_index_columns(index_name))});\n\n"
+            else:
+                ddl += f"CREATE UNIQUE INDEX {index_name} ON {database}.{table} ({','.join(self.get_index_columns(index_name))});\n\n"
+
+        return ddl
+
+    def get_indexes(self, table, database):
+        return self.query(f"select INDEX_NAME, UNIQUENESS from all_indexes  where upper(table_name) = upper('{table}') and upper(owner) = upper('{database}')")
+
+    def get_index_columns(self, index):
+        return self.query(f"select COLUMN_NAME from all_ind_columns  where INDEX_NAME = '{index}'").get_col(0)
+
+
+    def column_list_for_ddl(self, metadata):
+        column_ddl = []
+        for column_name, data_type, length, precision, scale, nullable in metadata:
+            if data_type == 'NUMBER':
+                if not precision:
+                    precision = 38
+                    scale = 0
+                column_ddl.append([column_name, 'DECIMAL({}, {})'.format(precision, scale)])
+            elif data_type == 'VARCHAR2':
+                column_ddl.append([column_name, 'VARCHAR({})'.format(length)])
+            elif data_type.startswith('TIMESTAMP'):
+                column_ddl.append([column_name, 'TIMESTAMP'])
+            elif data_type == 'DATE':
+                column_ddl.append([column_name, 'DATE'])
+            elif data_type == 'CHAR':
+                column_ddl.append([column_name, 'CHAR({})'.format(length)])
+            else:
+                raise RuntimeError('New data type found: {}'.format(data_type))
+
+            if nullable == 'N':
+                column_ddl[-1][-1] += ' NOT NULL'
+
+        return column_ddl
+
+    def get_ddl_from_meta(self, table, database) -> str:
         ddl = str(self.query(f"SELECT dbms_metadata.get_ddl('TABLE', '{table.upper()}', '{database.upper()}') FROM dual")[0][0]) + '\n;\n'
 
         for index in self.query(f"SELECT distinct index_name FROM all_ind_columns where table_name = '{table.upper()}' and table_owner = '{database.upper()}'").get_col(0):
@@ -89,6 +159,7 @@ class CxOracle(Database):
 
         return ddl
 
+
 if __name__ == '__main__':
     import sys
 
@@ -96,5 +167,6 @@ if __name__ == '__main__':
     sql.connect(sys.argv[1])
     print(sql.get_version())
     print(sql.get_current_db())
-    print(sql.get_tables('cs_orp'))
-    print(sql.get_ddl('f_cs_request', 'cs_orp'))
+    # print(sql.get_tables('supt_user'))
+    print(sql.get_ddl('ebay_policy', 'supt_user'))
+
